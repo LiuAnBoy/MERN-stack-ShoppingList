6d218df373ace6207a67f5630b98656bd5926c9c
unknown <eric141886@gmail.com>
2020-07-04 04:34:13 +0800

    version 1

---
+++ build/precache-manifest.4d61b3001bdc800b0bd54ef34bdd6c44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f13e0fdfbde562d1e8207f9f0b96df7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecf5eecfbf87ec0abe6b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "023ad45b95f608cc9f11",
+    "url": "/static/css/main.1103beab.chunk.css"
+  },
+  {
+    "revision": "ecf5eecfbf87ec0abe6b",
+    "url": "/static/js/2.d2a599ab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d2a599ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023ad45b95f608cc9f11",
+    "url": "/static/js/main.6243263d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
